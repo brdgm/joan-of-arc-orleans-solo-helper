ca4e009a21377b6620c6a3d716206ec3e7b16a70
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-07-02 19:55:44 +0200

    switch to vite

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,8 +5,9 @@ import SetupGame from '@/views/SetupGame.vue'
 import RoundTurn from '@/views/RoundTurn.vue'
 import EndOfGame from '@/views/EndOfGame.vue'
 import createRouter from 'brdgm-commons/src/util/router/createRouter'
+import { name } from '@/../package.json'
 
-const LOCALSTORAGE_KEY = process.env.VUE_APP_LOCALSTORAGE_KEY_PREFIX + "route"
+const LOCALSTORAGE_KEY = `${name}.route`
 
 const routes: Array<RouteRecordRaw> = [
   {
